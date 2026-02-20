2bf85bdea20ff0f800448a271f31fcc823d31671
Arjun <thefinanceschoolindiatech@gmail.com>
2026-02-20 21:23:45 +0000

    apiKey: "AIzaSyBiyWHaCknw3Vv5ONCAL2cINUDm6o79Y78",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6721629864-6b462",
-  "appId": "1:758460466831:web:7a936ed52d6ec3c44212fa",
-  "apiKey": "AIzaSyBiyWHaCknw3Vv5ONCAL2cINUDm6o79Y78",
-  "authDomain": "studio-6721629864-6b462.firebaseapp.com",
-  "storageBucket": "studio-6721629864-6b462.firebasestorage.app",
-  "measurementId": "",
-  "messagingSenderId": "758460466831"
+  apiKey: "AIzaSyBiyWHaCknw3Vv5ONCAL2cINUDm6o79Y78",
+  authDomain: "studio-6721629864-6b462.firebaseapp.com",
+  databaseURL: "https://studio-6721629864-6b462-default-rtdb.firebaseio.com",
+  projectId: "studio-6721629864-6b462",
+  storageBucket: "studio-6721629864-6b462.firebasestorage.app",
+  messagingSenderId: "758460466831",
+  appId: "1:758460466831:web:7a936ed52d6ec3c44212fa"
 };
